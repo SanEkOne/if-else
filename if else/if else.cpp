029400aab3363655fde05e6edac3cd2fd6d69aa3
Ostap_yd89 <Ostap_yd89@21-2>
2024-11-06 18:59:18 +0200

    Add project files.

---
+++ if else/if else.cpp
@@ -0,0 +1,131 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	//1
+	int resoult = 0;
+	cout << "1 = true, 0 = false\n";
+	cout << "2+2=4?\n";
+	cin >> resoult;
+	if (resoult == 1) {
+		cout << "RIGHT\n";
+	}
+	else if (resoult == 0) {
+		cout << "WRONG\n";
+	}
+	//2
+	int resoult2 = 0;
+	cout << "2+2=4?\n";
+	cin >> resoult2;
+	if (resoult2 == 1) {
+		cout << "RIGHT\n";
+	}
+	else if (resoult2 == 0) {
+		cout << "WRONG\n";
+	}
+	//3
+	int resoult3 = 0;
+	cout << "2+2=4?\n";
+	cin >> resoult3;
+	if (resoult3 == 1) {
+		cout << "RIGHT\n";
+	}
+	else if (resoult3 == 0) {
+		cout << "WRONG\n";
+	}
+	//4
+	int resoult4 = 0;
+	cout << "2+2=4?\n";
+	cin >> resoult4;
+	if (resoult4 == 1) {
+		cout << "RIGHT\n";
+	}
+	else if (resoult4 == 0) {
+		cout << "WRONG\n";
+	}
+	//5
+	int resoult5 = 0;
+	cout << "2+2=4?\n";
+	cin >> resoult5;
+	if (resoult5 == 1) {
+		cout << "RIGHT\n";
+	}
+	else if (resoult5 == 0) {
+		cout << "WRONG\n";
+	}
+	//6
+	int resoult6 = 0;
+	cout << "2+2=4?\n";
+	cin >> resoult6;
+	if (resoult6 == 1) {
+		cout << "RIGHT\n";
+	}
+	else if (resoult6 == 0) {
+		cout << "WRONG\n";
+	}
+	//7
+	int resoult7 = 0;
+	cout << "2+2=4?\n";
+	cin >> resoult7;
+	if (resoult7 == 1) {
+		cout << "RIGHT\n";
+	}
+	else if (resoult7 == 0) {
+		cout << "WRONG\n";
+	}
+	//8
+	int resoult8 = 0;
+	cout << "2+2=4?\n";
+	cin >> resoult8;
+	if (resoult8 == 1) {
+		cout << "RIGHT\n";
+	}
+	else if (resoult8 == 0) {
+		cout << "WRONG\n";
+	}
+	//9
+	int resoult9 = 0;
+	cout << "2+2=4?\n";
+	cin >> resoult9;
+	if (resoult9 == 1) {
+		cout << "RIGHT\n";
+	}
+	else if (resoult9 == 0) {
+		cout << "WRONG\n";
+	}
+	//10
+	int resoult10 = 0;
+	cout << "2+2=4?\n";
+	cin >> resoult10;
+	if (resoult10 == 1) {
+		cout << "RIGHT\n";
+	}
+	else if (resoult10 == 0) {
+		cout << "WRONG\n";
+	}
+	//11
+	int resoult11 = 0;
+	cout << "2+2=4?\n";
+	cin >> resoult11;
+	if (resoult11 == 1) {
+		cout << "RIGHT\n";
+	}
+	else if (resoult11 == 0) {
+		cout << "WRONG\n";
+	}
+	//12
+	int resoult12 = 0;
+	cout << "2+2=4?\n";
+	cin >> resoult12;
+	if (resoult12 == 1) {
+		cout << "RIGHT\n";
+	}
+	else if (resoult12 == 0) {
+		cout << "WRONG\n";
+	}
+
+
+
+
+}
